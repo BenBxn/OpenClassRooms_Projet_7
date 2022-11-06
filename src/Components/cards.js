e8d76792998ca088f7f38973c053ed59db2e44e7
BenBxn <dessortbxn@gmail.com>
2022-11-06 15:19:45 +0100

    Maj Responsive

--- src/Components/cards.js
+++ src/Components/cards.js
@@ -4,7 +4,10 @@ import "./cards.css";
 function Cards({id, image, titre}) {
     return(
         <div className="cards" id={id}>
+            <div className="gradients"></div>
+
             <img className="card-image" src={image} alt="card logement"/>
+
             <span className="card-titre">{titre}</span>
         </div>
     );
